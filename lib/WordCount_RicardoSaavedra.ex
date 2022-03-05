475a5c1cc59026922c10b093c2eb5077c2d965ab
Rass22 <100615564+Rass22@users.noreply.github.com>
2022-03-05 08:37:32 -0600

    Update WordCount_RicardoSaavedra.ex

--- lib/WordCount_RicardoSaavedra.ex
+++ lib/WordCount_RicardoSaavedra.ex
@@ -1,7 +1,7 @@
 defmodule WordCount do
   def count() do
     {:ok, text} = File.read("./Escritorio/Bunsan-Elixir/hola.txt") #read the file xd
-    text=String.replace(text,"á","a") #replace the word with the accent
+    text=String.replace(text,"á","a") #replace "á" with "a"
     text1=String.split(text) #Can split a string into multiple strings given a patterns (can be a list for example)
 
     Enum.frequencies(text1) # Return a map with keys as unique element of enumerable and values as the count of every element
